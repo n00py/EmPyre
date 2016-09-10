f375d6e986b069f230990eb7591e311bc19c3986
n00py <esteban.m.rodriguez@gmail.com>
2016-09-10 13:05:49 -0700

    Update mail.py
    Just changed the trigger word

--- lib/modules/persistence/osx/mail.py
+++ lib/modules/persistence/osx/mail.py
@@ -65,7 +65,7 @@ class Module:
             'Trigger' : {
                 'Description'   :   'The trigger word.',
                 'Required'      :   True,
-                'Value'         :   'Harambe'
+                'Value'         :   'n00py'
             }
         }
 #
